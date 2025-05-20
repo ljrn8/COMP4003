a339d7f4844f5e8b087d0613dc4b4b158d383b3f
Joel <119781027+ljrn8@users.noreply.github.com>
2025-05-20 23:32:35 +0800

    .

--- src/config.py
+++ src/config.py
@@ -10,6 +10,7 @@ load_dotenv(dotenv_path=dotenv_path)
 
 UNSWNB15_ROOT = Path(os.getenv("UNSWNB15_ROOT"))
 CICIDS2017_ROOT = Path(os.getenv("CICIDS2017_ROOT"))
+# BOTIOT_ROOT = Path(os.getenv("BOTIOT_ROOT"))
 
 LOG_LEVEL = os.getenv("LOG_LEVEL") or "INFO"
 
@@ -18,4 +19,5 @@ ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODEL_DIR = Path(ROOT) / "models"
 FIGURES_DIR = Path(ROOT) / "figures"
 INTERM_DIR = Path(ROOT) / "interm"
+DATA_ROOT = Path(ROOT) / "IDS datasets"
 ... # others
